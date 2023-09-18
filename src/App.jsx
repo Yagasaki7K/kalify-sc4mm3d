62df1a4786b18aca56406fe711fb57992101360f
Anderson Marlon <anderson18.marlon@gmail.com>
2023-09-18 01:23:21 -0300

    Removed Button + Filter Works

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,62 @@
 import styled from 'styled-components';
-import unSpammed from './links';
-import { useState } from 'react';
+import listURLs from './links';
+import { useState, useEffect, useCallback } from 'react';
 
 function App() {
     const [getLink, setGetLink] = useState('');
     const [message, setMessage] = useState('Check that your link is not tampered with');
 
-    function removeUnnecessaryUrl(url) {
-        const lastBarIndex = url.lastIndexOf('/');
-        if (lastBarIndex !== -1) {
-            return url.substring(0, lastBarIndex);
+    function filterDomains(url) {
+        const dominioRegex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?]+)/g;
+        const dominioMatch = dominioRegex.exec(url);
+
+        if (dominioMatch && dominioMatch.length > 1) {
+            return dominioMatch[1];
         }
-        return url;
+
+        return null;
     }
 
-    function checkLink() {
-        const urlWithoutBar = removeUnnecessaryUrl(getLink);
-        console.log(urlWithoutBar)
+    const newDomain = filterDomains(getLink).toLowerCase();
 
-        if (unSpammed.includes(urlWithoutBar)) {
+    console.log(newDomain); // Output: link.com.br
+
+    const checkLink = useCallback(() => {
+
+        if (newDomain === '' || newDomain === null) {
+            setMessage('Check that your link is not tampered with');
+            document.querySelector('#logo').style.color = '#fff';
+        } else if (listURLs.includes(newDomain)) {
             setMessage('Keep calm, this link is safe!');
             document.querySelector('#logo').style.color = '#70db90';
         } else {
             setMessage('Hey! Be careful, this link is strange and could be a scam!');
             document.querySelector('#logo').style.color = '#db7070';
         }
-    }
+    }, [newDomain]);
+
+    useEffect(() => {
+        checkLink();
+    }, [checkLink]);
 
     return (
         <MainDetails>
             <h1 id="logo">SP4MM3D</h1>
             <p>{message}</p>
 
-            <form onSubmit={(e) => e.preventDefault()}>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    checkLink();
+                }}
+            >
                 <input
                     type="text"
                     name="input"
-                    id="input"
-                    onChange={(e) => setGetLink(e.target.value)}
                     placeholder='https://www.amazon.com.br/Echo-Dot-5%C2%AA-gera%C3%A7%C3%A3o-Cor-Preta/dp/B09B8VGCR8/'
+                    value={getLink}
+                    onChange={(e) => setGetLink(e.target.value)}
                 />
-                <button type="button" onClick={checkLink}>SP4MM3D?</button>
             </form>
         </MainDetails>
     );
